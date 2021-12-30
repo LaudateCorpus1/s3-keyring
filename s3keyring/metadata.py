33856de098e1a947fa3443889a047ce05e3c58bd
Bill Medernach <bill.medernach@adroll.com>
2021-12-30 10:30:32 -0500

    WIP

--- s3keyring/metadata.py
+++ s3keyring/metadata.py
@@ -8,7 +8,7 @@ Information describing the project.
 package = 's3keyring'
 project = "S3 backend for the keyring module"
 project_no_spaces = project.replace(' ', '')
-version = '0.8.0.post3'
+version = 'keyringupgrade-0'
 description = 'Keeps your secrets safe in S3'
 authors = ['German Gomez-Herrero', 'Adroll']
 authors_string = ', '.join(authors)
--- setup.py
+++ setup.py
@@ -257,7 +257,7 @@ setup_dict = dict(
     package_data = {'': ['*.ini']},
     install_requires=[
         'click>=5.1',
-        'keyring==9.1',
+        'keyring>=9.1,<19',
         'boto3>=1.4.4',
         'awscli>=1.11.38',
         'botocore>=1.5.1',
